ea1df651b166e1235d651b21cc571f5cabd1b7a6
DongLee <sriharsha.g15@iiits.in>
2018-05-04 15:55:08 +0530

    Update urls.py

--- awesomesite/urls.py
+++ awesomesite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-#somethings not right here either
+#somethings not right here either. What are you waiting for? go ahead and fix it!
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
